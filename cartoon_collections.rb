2d3a64a620da8f0c0e56ffd8c362fd13fb1fbd48
Jas Spencer <jas.spencer414@gmail.com>
2019-09-05 15:03:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,11 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
+  laneteer_calls.any? { |call| call.length > 4 }
+  end
+  
+  long_planeteer_calls(planeteer_calls)
   # Your code here
 end
 
