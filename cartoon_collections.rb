cc25b4c2301cc3f27a5d633bbb634e8eff5db79f
Jas Spencer <jas.spencer414@gmail.com>
2019-09-05 15:04:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def summon_captain_planet(planeteer_calls)
 
   # code an argument here
   # Your code here
-end
+
 
 def long_planeteer_calls(long_planeteer_calls)# code an argument here
   laneteer_calls.any? { |call| call.length > 4 }
@@ -37,7 +37,6 @@ def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
